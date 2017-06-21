3f39ce1306e687ce376960b217a3904b7e89f3fc
Takatoshi Matsumoto <toqoz403@gmail.com>
2017-06-21 22:29:04 +0900

    alt+enter :)

--- FictionalWeekTest/WeeksTests.cs
+++ FictionalWeekTest/WeeksTests.cs
@@ -14,7 +14,7 @@ namespace FictionalWeekTest
             Assert.That (w.Month (), Is.EqualTo (1));
             Assert.That (w.Week (), Is.EqualTo (1));
             // 12*4-1 進める (= 12*4 週目)
-            for (int i = 0; i < 12 * 4 - 1; i++) {
+            for (var i = 0; i < 12 * 4 - 1; i++) {
                 w.Next ();
             }
             Assert.That (w.Year (), Is.EqualTo (1));
@@ -26,9 +26,9 @@ namespace FictionalWeekTest
             Assert.That (w.Month (), Is.EqualTo (1));
             Assert.That (w.Year (), Is.EqualTo (2));
 
-            for (int x = 0; x < 4; x++) {
+            for (var x = 0; x < 4; x++) {
                 // 12*4-1 進める (= 12*4 週目)
-                for (int y = 0; y < 12 * 4; y++) {
+                for (var y = 0; y < 12 * 4; y++) {
                     w.Next ();
                 }
             }
@@ -53,7 +53,7 @@ namespace FictionalWeekTest
         public void TestWeeks_week50()
         {
             var w = new Weeks ();
-            for (int i = 0; i < 49; i++) {
+            for (var i = 0; i < 49; i++) {
                 w.Next ();
             }
             Assert.That (w.Month (), Is.EqualTo (1));
@@ -63,7 +63,7 @@ namespace FictionalWeekTest
         public void TestWeeks_week53()
         {
             var w = new Weeks ();
-            for (int i = 0; i < 52; i++) {
+            for (var i = 0; i < 52; i++) {
                 w.Next ();
             }
             Assert.That (w.Month (), Is.EqualTo (2));
